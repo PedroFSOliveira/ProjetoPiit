b61a4f3a54f268981dfc03fc2bc201c470cdfb88
Lancelloth <thibmoliveira@gmail.com>
2017-07-20 20:26:31 -0300

    Commit 2

--- DefVersionPiit/App_Start/RouteConfig.cs
+++ DefVersionPiit/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace DefVersionPiit
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //rotas
+            //rotas Thiago teste 1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
